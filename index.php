ca9069f5206af750789ad408c4e23122ed083a6d
riskabima-png <riskabima@gmail.com>
2026-04-13 13:17:38 +0800

    commit pertamaku

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$nama ="abdulla";
+$umur = 20;
+$tinggi = 170.5;
+$menikah = true;
+$hobi =["membaca", "bermain", "bermain gitar"];
+
+echo "Nama saya $nama, umur saya $umur, tinggi saya $tinggi, status saya $menikah, hobi saya $hobi[0], $hobi[1], $hobi[2]";
+?>
