656c972c1d746176ca1eff6416d7d360fda790ee
riskabima-png <riskabima3>
2026-04-16 08:57:15 +0800

    index.php

--- index.php
+++ index.php
@@ -7,3 +7,31 @@ $hobi =["membaca", "bermain", "bermain gitar"];
 
 echo "Nama saya $nama, umur saya $umur, tinggi saya $tinggi, status saya $menikah, hobi saya $hobi[0], $hobi[1], $hobi[2]";
 ?>
+
+echo "<br><br>==============================================<br><br>";
+
+//OPERATOR
+$nllail  = 5;
+$nilla12 = 8;
+$nilla13 = 10;
+$nilla14 = 20;
+$nilla15 = 13;
+
+$hasil = $nilail + $nilail2 - $nilail3 *$nilla14 /$nilla15 adalah hasil";
+
+//percabangan 
+
+$nilai = 70;
+
+if ($nilai >= 80) {
+   echo "Grada A";
+} else if ($nilai >= 70) {   
+   echo "Grada B";
+} else if ($nilai >= 50) {
+    echo "Grada C";
+} else if ($nilai >= 30) {
+    echo "Grada D";
+} else {
+    echo "Grade E"
+}
+
